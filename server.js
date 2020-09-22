8bc0500221032cec7a49e888479a6baa096f3c8a
salyadav <39609162+salyadav@users.noreply.github.com>
2020-09-22 19:40:50 +0530

    adding a test api call

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express  = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
-// const serveStatic = require('serve-static');
-const serverless = require('serverless-http');
 
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-const BASE_URL = "/.netlify/functions/";
+const BASE_URL = "/";
 
 app.use(cors());
 app.use(express.json());
@@ -32,9 +30,11 @@ const feedbackRouter = require(path.join(__dirname, './routes/feedback'));
 app.use(BASE_URL + 'feedback', feedbackRouter);
 app.use(BASE_URL + 'leaderboard', leaderboardRouter);
 
+app.Router().get(BASE_URL, (req, res) => res.json({
+    test: "Test api call successful"
+}));
+
 //server start
-// app.listen(port, () => {
-//     console.log(`Server is running on ${port}`);
-// });
-module.exports = app;
-module.exports.handler = serverless(app);
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+});
